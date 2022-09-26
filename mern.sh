8739eb4e840a1f282908e647af0d23f3d5165157
Pankaj Singh <pkstiyara@gmail.com>
2022-09-26 16:41:38 +0530

    Update mern.sh

--- mern.sh
+++ mern.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+#Date : 26 September 2022
+#Author : Pankaj Kumar Singh
+#Purpose : Ready a MEAN Stack ( MongoDB, Express, ANgular, Node.js)
+# This is widely used server in the world.
 
+    # MongoDB (document database)
+    # Express (back-end application framework)
+    # React (front-end application framework)
+    # Node.js (JavaScript runtime environment)
+
+# This framework comes as an alternative to the widely known LAMP stack, 
+# which uses JS on the client side, but PHP on the server side.
+
+
+
+echo “Your installation is started”
+
+# updating the system
+sudo apt-get update
+sudo apt-get upgrade
