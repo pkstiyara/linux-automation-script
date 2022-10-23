2a5c14134c776e84549e37be21d6d2ef0915a454
Pankaj Singh <pkstiyara@gmail.com>
2022-10-23 11:58:08 +0530

    Tomcat server installation on Ubuntu

---
+++ apache-tomcat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Date : 23-October -2022
+#Author : Pankaj Kumar Singh
+#Purpose : This script is used for installing a apache tomcat server on Ubuntu Linux machine.
+
+echo “Your installation is started”
+
+# updating the system
+sudo apt-get update && sudo apt-get upgrade -y
